97d1edf8f4c045a4d1e91b3037fb9b7603b0dd14
esmyers <emyers@usgs.gov>
2019-08-08 12:38:12 -0500

    changed to huc12 as default layer on startup

--- src/scripts/event-handlers.js
+++ src/scripts/event-handlers.js
@@ -64,6 +64,8 @@ function loadEventHandlers() {
     /* GROUP RESULTS (AGGREGATE LAYER) EVENTS */
     //set initial Displayed Metric options
     $("#groupResultsSelect").on("loaded.bs.select", function() {
+        $("#groupResultsSelect").selectpicker('val', groupResultsLabels.b); //set to GRP3 for initial load and refresh
+        $("#groupResultsSelect").selectpicker('refresh');
         populateMetricOptions($("#groupResultsSelect")[0].selectedIndex);
         updateSourceOptions();
         generateRenderer();
